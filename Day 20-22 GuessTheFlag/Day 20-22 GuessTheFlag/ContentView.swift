fcff02437a649cbcbb94e4898eb64bde5e946b9f
Gienadij Mackiewicz <gienadijmackiewicz@gmail.com>
2019-10-18 22:44:03 +0200

    Day 22

--- Day 20-22 GuessTheFlag/Day 20-22 GuessTheFlag/ContentView.swift
+++ Day 20-22 GuessTheFlag/Day 20-22 GuessTheFlag/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
     @State private var countries: [String] = ["Estonia",
                                               "France",
                                               "Germany",
-                                              "Irleand",
+                                              "Ireland",
                                               "Italy",
                                               "Nigeria",
                                               "Poland",
@@ -26,24 +26,28 @@ struct ContentView: View {
     @State var correctAnswer: Int = Int.random(in: 0...2)
     @State private var showingScore: Bool = false
     @State private var scoreTitle: String = ""
+    @State private var score: Int = 0
+    @State private var selectedCountry: String = ""
     
     // MARK: - Body
     
     var body: some View {
         ZStack {
             LinearGradient(gradient: .init(colors: [.blue, .black]),
-                           startPoint: .top,
-                           endPoint: .bottom).edgesIgnoringSafeArea(.all)
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing).edgesIgnoringSafeArea(.all)
             
-            VStack(spacing: 30) {
-                VStack(spacing: 5) {
-                    Text("Tap the flag of")
+            VStack(spacing: 25) {
+                VStack {
+                    Text("Tap the flag of:")
+                        .font(.headline)
                         .foregroundColor(.white)
+                        .padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10))
                     Text(countries[correctAnswer])
                         .foregroundColor(.white)
                         .font(.largeTitle)
                         .fontWeight(.black)
-                }
+                }.lineLimit(nil)
                 ForEach(0..<3) { number in
                     Button(action: {
                         self.flagTapped(number)
@@ -55,11 +59,17 @@ struct ContentView: View {
                     }
                 }
                 Spacer()
+                ZStack {
+                    Color(#colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0.3016909247))
+                    Text("Score: \(score)")
+                        .foregroundColor(.white)
+                        .font(.system(size: 22, weight: .medium))
+                }.frame(maxHeight: 50)
             }
         }
         .alert(isPresented: $showingScore) {
             Alert(title: Text("\(scoreTitle)"),
-                  message: Text("Your score is not available yet"),
+                  message: Text("Your current score: \(score)"),
                   dismissButton: .default(Text("OK")){
                     self.askQuestion()
                 })
@@ -69,10 +79,15 @@ struct ContentView: View {
     // MARK: - Private methods
     
     func flagTapped(_ number: Int) {
+        selectedCountry = countries[number]
         if number == correctAnswer {
-            scoreTitle = "Correct"
+            score += 1
+            scoreTitle = "Correct!"
         } else {
-            scoreTitle = "Wrong, try again!"
+            if score > 0 {
+                score -= 1
+            }
+            scoreTitle = "Wrong! That's the flag of \(selectedCountry)"
         }
         
         showingScore = true
